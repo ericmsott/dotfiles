9e835a57cb03947747859c87d93f90dd8f2d822a
ericmsott <32421933+ericmsott@users.noreply.github.com>
2021-03-07 17:06:56 -0300

    Add files via upload

---
+++ herbstluftwm/panel.sh
@@ -0,0 +1,19 @@
+ #/bin/bash
+
+ #A simple popup showing system information
+
+ HOST=$(uname -n)
+ KERNEL=$(uname -r)
+ UPTIME=$( uptime | sed 's/.* up //' | sed 's/[0-9]* us.*//' | sed 's/ day, /d /'\
+          | sed 's/ days, /d /' | sed 's/:/h /' | sed 's/ min//'\
+            |  sed 's/,/m/' | sed 's/  / /')
+ PACKAGES=$(pacman -Q | wc -l)
+ UPDATED=$(awk '/upgraded/ {line=$0;} END { $0=line; gsub(/[\[\]]/,"",$0); \
+          printf "%s %s",$1,$2;}' /var/log/pacman.log)
+
+ (
+ echo "System Information - Host: $HOST - Kernel: $KERNEL - Uptime: $UPTIME - Pacman: $PACKAGES packages - Last updated on: $UPDATED"
+ ) | dzen2 -p -x "0" -y "0" -w "1478" -h "16" -sa 'l' -ta 'c'\
+    -title-name 'popup_sysinfo' -e 'onstart=uncollapse;button1=exit;button3=exit'
+
+ # "onstart=uncollapse" ensures that slave window is visible from start.
